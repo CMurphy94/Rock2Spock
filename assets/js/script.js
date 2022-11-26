4627de163b72c66310984275811ce0a4c4458c68
CMurphy94 <conormurphy94@hotmail.com>
2022-11-26 21:22:16 +0000

    Added additional css styling and modal size for smaller screens

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,34 @@
+document.addEventListener("DOMContentLoaded", openInstructions);
+
 /*
- * Instructions on how to play the game code from W3Schools, directly credited in ReadMe
+* Instructions on how to play the game code from W3Schools, directly credited in ReadMe
  */
+function openInstructions () {
 // Get the modal
- var modal = document.getElementById("myModal");
+ const modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("instBtn");
+ const btn = document.getElementById("instBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+ const span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+ btn.onclick = function() {
+   modal.style.display = "block";
+ }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+ span.onclick = function() {
+   modal.style.display = "none";
+ }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+ window.onclick = function(event) {
+   if (event.target == modal) {
+     modal.style.display = "none";
+   }
+ }
 }
 
 /**
